23d4264e09fc3c1854a3b7ff4bde06b8fe4f1223
adamkkarl <adamkkarl@gmail.com>
2022-06-09 12:22:28 -0400

    determines colors of each letter for the given guess, including duplicate support

--- wordlesolver.py
+++ wordlesolver.py
@@ -3,76 +3,136 @@
 import re
 
 wordList = []
-words = ""
+numWords = 0
 
-#keep track of exactly what info we have on the word
-green = "....." #found in this position (green)
-yellow = ['','','','',''] #found in word but not at this position (yellow)
-gray = "" #not found any additional times in the word (gray)
+answer = ''
+guesses = ['','','','','', ''] # holds the 5-letter guesses
+colors  = [None, None, None, None, None, None] # 0=gray, 1=yellow, 2=green
+
+#example: first guess 'owing' when answer is 'wring'
+#guesses = ['owing','','','','', '']
+#colors  = ['01222','','','','', '']
 
 def numMatching():
     """Given all clues so far, return the number of words the answer could be"""
-    #narrow down initially based on green letters using regex
-    cut1 = re.findall(green, words)
+    global wordList, numWords
 
-    #narrow down based on yellow letters
-    cut2 = list()
-    for word in cut1:
-        test = True
-        for pos in range(5):
-            for c in yellow[pos]:
-                #test if yellow character IS in word but NOT at this position
-                if c in green:
-                    letterFoundElseWhere = False
-                    for i in range(5):
-                        if i != pos and green[i] != c and word[i] == c:
-                            #character found in the word
-                            letterFoundElseWhere = True
-                elif c not in word or word[pos] == c:
-                    test = False
-        if test == True:
-            cut2.append(word)
-
-    #finally, narrow based on grey letters
-    #keep in mind that duplicate letters may
-    cut3 = list()
-    for word in cut2:
-        test = True
-        for c in gray:
-            if c in green:
-                for i in range(5):
-                    if green[i] != c and word[i] == c:
-                        test = False
-            elif c in word:
-                test = False
-        if test == True:
-            cut3.append(word)
-
-    print(cut3)
-    return len(cut3)
+    remaining = list()
+    for word in wordList:
+        valid = True
+        for guess in guesses:
+            for pos in range(5):
+                if valid:
+                    if guess[pos] == '2' and word[pos] != guess[pos]:
+                        #guess doesn't match established green
+                        valid = False
+                    elif guess[pos] == '1':
+                        #guess has inva
+                        pass
 
 
-def main():
+        if valid:
+            remaining.append(word)
+
+    print(remaining)
+    print(f"{len(remaining)}", flush=True)
+    return len(remaining)
+
+def guess(myGuess, numGuess):
+    """add the given word to the list of guesses and determine the colors of the letters
+    input: the word and the guess number (0 for first guess up to 5 for 6th and final guess)"""
+    global guesses, colors, answer
+
+    guesses[numGuess] = myGuess
+    tmp = [0,0,0,0,0] #start with all letters grayed out
+
+    #determine greens
+    for pos in range(5):
+        if myGuess[pos] == answer[pos]:
+            tmp[pos] = 2
+
+    #determine yellows
+    #for each position that isn't already green, the first occurrance of the character
+    #that isn't yellow or green should be changed to yellow
+    for pos in range(5):
+        if tmp[pos] != 2:
+            c = answer[pos]
+            for i in range(5):
+                if tmp[i] == 0 and myGuess[i] == c:
+                    tmp[i] = 1
+                    break
+
+    colors[numGuess] = tmp
+    return
+
+def loadFile():
+    global wordList, numWords
 
     f = open(r'sgb-words.txt', 'r')
     wordList = f.readlines()
-    numWords = len(wordList)
-    print(f"{numWords} 5-letter words in dictionary")
     f.close()
+
+    numWords = len(wordList)
+    for i in range(numWords): #trim whitespace
+        wordList[i] = wordList[i][:5]
+    return
+
+def main():
+    global wordList, numWords, answer
+
+    loadFile()
+
+    print(f"{numWords} 5-letter words in dictionary")
 
     f = open(r'sgb-words.txt', 'r')
     words = f.read()
+    print(words, flush=True)
 
-    regex = "..ing"
-    x = re.findall(regex, words)
-    print(f"{len(x)} words match the regex \"{regex}\"")
+    remaining = [0 for i in range(numWords)]
+    # for i in range(numWords):
+    #     answer = wordList[i]
+    #     for j in range(numWords):
+    #         guess = wordList[j]
+    #         green = ''
+    #         yellow = ['a','','','','']
+    #         gray = ''
+    #         for pos in range(5):
+    #             #determine greens
+    #             if answer[pos] == guess[pos]:
+    #                 green = green + answer[pos]
+    #             else:
+    #                 green = green + '.'
+    #
+    #             #determine yellows
+    #             if guess[pos] != answer[pos] and guess[pos] in answer:
+    #                 yellow[pos] = yellow[pos] + guess[pos]
+    #
+    #             #determine grays
+    #             if guess[pos] not in guess:
+    #                 gray = gray + guess[pos]
+    #
+    #         avgRemaining[j] += numMatching()
 
-#    for i in range(numWords):
-#        answer = wordList[i]
-#        for j in range(numWords):
+    avgRemaining = [x/numWords for x in remaining]
+
+    bestAvg = min(avgRemaining)
+    bestWord = wordList[avgRemaining.index(bestAvg)]
+
+    print(f'{bestWord} is the best initial guess with an avg of {bestAvg} words remaining')
 
 
     f.close()
+
+
+def tmp():
+    global answer, guesses, colors
+
+    loadFile()
+
+    answer = 'aaaxx'
+    guess('xxxhh', 0)
+    print(guesses)
+    print(colors)
 
 
 if __name__ == "__main__":
