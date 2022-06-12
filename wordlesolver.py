9207389ace90168467489c1aa90a16be4e9fa15e
adamkkarl <adamkkarl@gmail.com>
2022-06-12 00:39:13 -0400

    pattern matrix solution works on small datasets but needs optimizing

--- wordlesolver.py
+++ wordlesolver.py
@@ -10,119 +10,63 @@ from numpy import uint8, dtype
 
 WORDS_FILE = 'sgb-words.txt'
 PATTERN_MATRIX_FILE = 'output_pattern_matrix.txt'
-patternMatrix = []
+patternMatrix = []  #patternMatrix[guessIndex][answerIndex] to find the pattern score
 
 wordList = []
 numWords = 0
 remaining = []
 
-# def validBasedOnGuess(myWord, prevGuess, colors):
-#     """given a word and a single previous guess + corresponding colors,
-#     return true if the word could possibly be a solution"""
-#     word = myWord
-#
-#     #check word matches green letters
-#     greenIndexes = list()
-#     for pos in range(5):
-#         if colors[pos] == 2:
-#             greenIndexes.append(pos)
-#             if word[pos] != prevGuess[pos]:
-#                 #doesn't match green letter
-#                 return False
-#
-#     #since all greens match, can eliminate to make problem simpler
-#     while len(greenIndexes) > 0:
-#         i = greenIndexes[-1]
-#         word = word[:i] + word[i+1:]
-#         prevGuess = prevGuess[:i] + prevGuess[i+1:]
-#         del colors[i]
-#         del greenIndexes[-1]
-#
-#     #solve yellows by iteratively simplifying yellow pairs
-#     #replace yellow char in prevGuess with '?'
-#     #replace first instance of that char in word with '!'
-#     for pos in range(len(prevGuess)):
-#         if colors[pos] == 1:
-#             c = prevGuess[pos]
-#             restOfWord = word[:pos] + word[pos+1:]
-#             if c not in restOfWord or word[pos] == c:
-#                 #yellow letter either found in same position or not at all
-#                 return False
-#             else:
-#                 prevGuess = prevGuess[:pos] + '?' + prevGuess[pos+1:]
-#                 wordPos = word.find(c)
-#                 word = word[:wordPos] + '!' + word[wordPos+1:]
-#
-#
-#     #finally, check for overlap in gray characters
-#     for pos in range(len(prevGuess)):
-#         if colors[pos] == 0:
-#             if prevGuess in word:
-#                 return False
-#
-#     return True
-#
-#
-# def numRemainingValidWords(answer, guessed, colors):
-#     """Given all clues so far, return the number of words the answer could still be"""
-#     global wordList
-#
-#     if guessed[0] == None:
-#         #no guesses made yet so whole dictionary is valid
-#         return wordList
-#
-#     remaining = list()
-#     for word in wordList:
-#         valid = True
-#         for i in range(6):
-#             if guessed[i] == None:
-#                 break
-#             elif validBasedOnGuess(word, guessed[i], colors[i].copy()) == False:
-#                     valid = False
-#                     break
-#         if valid:
-#             remaining.append(word)
-#
-#     return len(remaining)
-#
-# def analyzeGuessesGivenAnswer(answer):
-#     """Given the answer, try all possible initial guesses and determine how
-#     much they narrow down the solution. Add to global based on the remaining
-#     solutions"""
-#     global wordList, numWords, remaining
-#
-#     for i in range(numWords):
-#         firstGuess = wordList[i]
-#         c = determineColors(answer, firstGuess)
-#         guessed = [firstGuess, None, None, None, None, None]
-#         colors  = [c, None, None, None, None, None]
-#         rem = numRemainingValidWords(answer, guessed, colors)
-#         remaining[i] += rem
-#
-#
-# def determineColors(answer, guess):
-#     """given the answer and a guess, determine the colors of the letters
-#     output is an array of 5 values, 0=gray, 1=yellow, 2=green"""
-#     #start with all letters grayed out
-#     colors = array.array('i', (0 for i in range(5)))
-#
-#
-#     #determine greens
-#     for pos in range(5):
-#         if guess[pos] == answer[pos]:
-#             colors[pos] = 2
-#
-#     #determine yellows
-#     #for each position that isn't already green, the first occurrence of the character
-#     #that isn't yellow or green should be changed to yellow
-#     for pos in range(5):
-#         if colors[pos] != 2:
-#             c = answer[pos]
-#             for i in range(5):
-#                 if colors[i] == 0 and guess[i] == c:
-#                     colors[i] = 1
-#                     break
-#     return colors
+def analyzeGuesses():
+    """Analyze all combinations of answer and first guess, each time determining
+    the number of remaining valid words the answer could be. Return an array of the
+    sum total remaining possibilities given an initial guess"""
+    global wordList, numWords, patternMatrix
+    
+    remaining = [0 for i in range(numWords)]
+
+    print('Starting first guess analysis...', flush=True)
+    startTime = timeit.default_timer()
+    for answerIndex in range(numWords):        
+        for firstGuessIndex in range(numWords):
+            firstGuessScore = patternMatrix[firstGuessIndex][answerIndex]
+            for potentialGuessIndex in range(numWords):
+                    #a potential guess could be the answer if
+                    #pattern of (prevGuess, answer) == pattern of (prevGuess, potentialGuess)
+                    if patternMatrix[firstGuessIndex][potentialGuessIndex] == firstGuessScore:
+                        remaining[firstGuessIndex] += 1
+            
+        #provide progress updates
+        currTime = timeit.default_timer()
+        elapsedTime = currTime - startTime
+        predTime = elapsedTime*(numWords/(answerIndex+1)) - elapsedTime
+        formatted_time = "{:.2f}".format(predTime/60)
+        lowestRem = min(remaining)
+        bestWord = wordList[remaining.index(lowestRem)]
+        print(f'{bestWord} is the best initial guess so far with {lowestRem/(answerIndex+1)} words remaining')
+        print(f"{answerIndex+1}/{numWords} answers analyzed, ~{formatted_time} mins remaining", flush=True)
+    return remaining
+
+def determinePattern(answer, guess):
+    """Given a guess and answer, determine a pattern for the guess where
+    0=gray, 1=yellow, 2=green"""
+    pattern = [0,0,0,0,0]
+
+    #determine greens
+    for pos in range(5):
+        if guess[pos] == answer[pos]:
+            pattern[pos] = 2
+
+    #determine yellows
+    #for each answer char that isn't already green, the *first* occurrence of the
+    #guess char that isn't yellow or green should be changed to yellow
+    for ansPos in range(5):
+        if pattern[ansPos] != 2:
+            c = answer[ansPos]
+            for guessPos in range(5):
+                if pattern[guessPos] == 0 and guess[guessPos] == c:
+                    pattern[guessPos] = 1
+                    break
+    return pattern
 
 def patternScore(colors):
     """input: list of 5 integers where 0=gray 1=yellow 2=green
@@ -135,7 +79,7 @@ def initializePatternMatrix():
     """Given the word list, create a 2D matrix of all patterns.
     with gray=0, yellow=1, green=2 at each of 5 positions, the pattern
     can be saved uniquely as an int between 0 and 3^5.
-    Use colorMatrix[guess][answer] to find the pattern"""
+    Use patternMatrix[guessIndex][answerIndex] to find the pattern score"""
     global wordList, numWords, patternMatrix
     
     print('Generating pattern matrix...', flush=True)
@@ -143,30 +87,14 @@ def initializePatternMatrix():
     tmp = [[0 for i in range(numWords)] for j in range(numWords)]
     patternMatrix = np.array(tmp, dtype=uint8)
     
-    
     for i in range(numWords):
         if i%100 == 0 and i>0:
             print(f'{i}/{numWords} pattern rows generated', flush=True)
         for j in range(numWords):
             guess = wordList[i]
             answer = wordList[j]
-            pattern = [0,0,0,0,0]
-
-            #determine greens
-            for pos in range(5):
-                if guess[pos] == answer[pos]:
-                    pattern[pos] = 2
-
-            #determine yellows
-            #for each answer char that isn't already green, the *first* occurrence of the
-            #guess char that isn't yellow or green should be changed to yellow
-            for ansPos in range(5):
-                if pattern[ansPos] != 2:
-                    c = answer[ansPos]
-                    for guessPos in range(5):
-                        if pattern[guessPos] == 0 and guess[guessPos] == c:
-                            pattern[guessPos] = 1
-                            break
+            
+            pattern = determinePattern(answer, guess)
             score = patternScore(pattern)
             patternMatrix[i][j] = uint8(score)
     
@@ -215,35 +143,19 @@ def main():
     global wordList, numWords, patternMatrix
 
     loadWordList()
-    remaining = [0 for i in range(numWords)]
-
+    makePatternMatrix()    
     print(f"{numWords} 5-letter words in dictionary", flush=True)
-
-    startTime = timeit.default_timer()
-    for i in range(numWords):
-        answer = wordList[i]
-        analyzeGuessesGivenAnswer(answer)
-
-        endTime = timeit.default_timer()
-        predTime = (endTime-startTime)*(numWords/(i+1))
-        formatted_time = "{:.2f}".format(predTime/3600)
-        print(f"{i+1}/{numWords} complete, ~{formatted_time} h remaining", flush=True)
-
+    
+    remaining = analyzeGuesses()
     avgRemaining = [x/numWords for x in remaining]
 
     bestAvg = min(avgRemaining)
     bestWord = wordList[avgRemaining.index(bestAvg)]
     print(f'{bestWord} is the best initial guess with an avg of {bestAvg} words remaining')
     
-
-
-def tmp():
-    loadWordList()
-    startTime = timeit.default_timer()
-    makePatternMatrix()
-    endTime = timeit.default_timer()
-    print(patternMatrix)
-    print(f"complete, ~{(endTime-startTime)/60} m elapsed", flush=True)
+    worstAvg = max(avgRemaining)
+    worstWord = wordList[avgRemaining.index(worstAvg)]
+    print(f'{worstWord} is the worst initial guess with an avg of {worstAvg} words remaining')
 
 if __name__ == "__main__":
-    tmp()
+    main()
